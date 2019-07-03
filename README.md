41c291fca385bd5b3f84449ae0f952afc3fa0b9f
Tony Kieling <tony.kieling@gmail.com>
2019-07-03 16:19:01 -0700

    first part, front is pretty done

---
+++ README.md
@@ -0,0 +1,8 @@
+Project to practice dealing picture stuff.  
+
+Dependencies:  
+1. NodeJs  
+2. Express  
+3. Postgres  
+4. React  
+5. 
---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import './App.css';
+import axios from 'axios';
+
+export default class App extends Component {
+  state = {
+    pictureFile: null
+  };
+
+  handler = event => {
+    // event.preventDefault();
+    const file = event.target.files[0];
+    if (file.size > (1024 * 1024 * 1)) {
+      alert("big file!")
+    }
+    else
+      this.setState({
+        pictureFile: file,
+        loaded: 0
+      });
+
+    }
+    
+  sendPicture = event => {
+    const data = new FormData() ;
+    data.append('file', this.state.pictureFile);
+
+    axios.post("http://localhost:8000/upload", data, { /*receive two parameter endpoint url ,form data*/ })
+      .then(res => { // then print response status
+        console.log(res.statusText)
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <input type="file" name="file" onChange={this.handler}/>
+
+        <button type="button" className="btn btn-success btn-block" onClick={this.sendPicture}>Upload</button> 
+      </div>
+    )
+  }
+}
